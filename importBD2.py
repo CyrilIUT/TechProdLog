8800b1eaa2413b97892b7989845bf8e3c8290904
Cyril Dernoncourt <cyril.dernoncourt@etu.univ-nantes.fr>
2017-03-23 10:51:23 +0100

    L'importation fonctionne enfin

--- importBD2.py
+++ importBD2.py
@@ -13,29 +13,29 @@ with open("CSV/equipements.csv", newline='') as csvfile:
 with open('CSV/installations_table.csv', newline='') as csvfile:
 	readerInstallation = csv.DictReader(csvfile)
 	for row in readerInstallation:
-			values = (row["Numero de l'installation"], row["Nom usuel de l'installation"], (row['Numero de la voie']+" "+row['Nom de la voie']), row['Code postal'], row['Nom de la commune'], row['Latitude'], row['Longitude'])
+			values = (row["Numéro de l'installation"], row["Nom usuel de l'installation"], (row['Numero de la voie']+" "+row['Nom de la voie']), row['Code postal'], row['Nom de la commune'], row['Latitude'], row['Longitude'])
 			cursor.execute('''INSERT INTO installation values(?,?,?,?,?,?,?)''',values)
 
 with open('CSV/equipements_activites.csv',newline='') as csvfile:
 	readerInstallation = csv.DictReader(csvfile)
 	for row in readerInstallation:
 		if(row['ActCode'] != ''):
+			donnes = (row['EquipementId'],row['ActCode'])
 			if(row['ActLib'] == ''):
 				values = (row['ActCode'],"Indefini")
 			else:
 				values = (row['ActCode'],row['ActLib'])
 
 			cursor.execute("SELECT * FROM activite where numAct =\'"+row['ActCode']+"\';")
-			row = cursor.fetchone()
+			ligne = cursor.fetchone()
 
-			if row is None:
+			if ligne is None:
 				cursor.execute('''INSERT INTO activite values (?,?)''',values)
 
-with open('CSV/equipements_activites.csv',newline='') as csvfile:
-	readerInstallation = csv.DictReader(csvfile)
-	for row in readerInstallation:
-		donnes = (row['EquipementId'],row['ActCode'])
-		cursor.execute('''INSERT INTO liaisonEquAct values (?,?)''',donnes)
+			cursor.execute("SELECT * FROM liaisonEquAct where numAct =\'"+row['ActCode']+"\' and numEqu =\'"+row['EquipementId']+"\';")
+			ligne2 = cursor.fetchone()
 
+			if ligne2 is None:
+				cursor.execute('''INSERT INTO liaisonEquAct values (?,?)''',donnes)
 
 conn.commit()
