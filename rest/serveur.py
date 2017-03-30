b1013f760cb6a43e788d06082f626dfe2e6ad571
Cyril Dernoncourt <cyril.dernoncourt@etu.univ-nantes.fr>
2017-03-30 10:10:31 +0200

    Creation du dossier qui servira au serveur

---
+++ rest/serveur.py
@@ -0,0 +1,7 @@
+from lib.bottle.bottle import route, run, template
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=9999)
